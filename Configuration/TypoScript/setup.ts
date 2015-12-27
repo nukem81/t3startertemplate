7b100e605f1e09b9dbd86e7a1e728e902e03ff53
Marco Malagoli <malagolim@gmail.com>
2015-12-27 11:50:24 +0100

    Removed the inclusion of 'CSS Styled Content'

--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -1,7 +1,6 @@
 #*****************************************************
 # Include TypoScript files
 #*****************************************************
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:css_styled_content/static/setup.txt">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3startertemplate/Configuration/TypoScript/config.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3startertemplate/Configuration/TypoScript/tt_content.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3startertemplate/Configuration/TypoScript/content.ts">
